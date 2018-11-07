4a35137c3a3300003ebad3b1b85acb6183306811
Max Arias <max@max-arias.com>
2018-11-07 15:23:25 -0300

    Initial work towards an exploration game

--- src/index.js
+++ src/index.js
@@ -8,9 +8,19 @@ import './less/main.less';
 import { GameScene } from './scenes/game';
 
 const gameConfig = {
+  type: Phaser.AUTO,
   width: 1024,
   height: 768,
-  scene: GameScene
+  scene: GameScene,
+  pixelArt: true,
+  zoom: 2,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 0 },
+        debug: false
+    }
+  },
 };
 
 new Phaser.Game(gameConfig);
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,7 +1,4 @@
 import 'phaser';
-import EasyStar from 'easystarjs';
-
-import { Building } from '../entities/building';
 import Player from '../entities/player';
 
 export class GameScene extends Phaser.Scene {
@@ -13,17 +10,21 @@ export class GameScene extends Phaser.Scene {
   preload() {
     this.scene = this;
     this.load.spritesheet('scifi_tilesheet_img', 'assets/tilemaps/scifi_tilesheet.png', { frameWidth: 64, frameHeight: 64 });
-    this.load.atlasJSONHash('player', 'assets/spritesheets/player.png', 'assets/spritesheets/player.json');
     this.load.tilemapTiledJSON('map', 'assets/tilemaps/scifimap.json');
+
+    // TODO: Make this one single atlas
+    this.load.spritesheet('player_idle', 'assets/spritesheets/player_idle.png', { frameWidth: 19, frameHeight: 21 });
+    this.load.spritesheet('player_walk', 'assets/spritesheets/player_walk.png', { frameWidth: 18, frameHeight: 20 });
   }
 
   create() {
     this.createAnimations();
-
-    this.player = new Player(this, 300, 300, {key: 'player'});
-    this.add.existing(this.player);
-
     this.setUpMap();
+
+    this.player = new Player(this, 0, 0, {key: 'player'});
+
+    this.physics.add.collider(this.player, this.background_layer);
+
     this.setUpCamera();
   }
 
@@ -32,45 +33,32 @@ export class GameScene extends Phaser.Scene {
   createAnimations() {
     this.anims.create({
       key: "player-idle",
-      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 12 }),
-      frameRate: 3,
+      frames: this.anims.generateFrameNumbers("player_idle", { start: 0, end: 12 }),
+      frameRate: 6,
       repeat: -1
     });
 
     this.anims.create({
       key: "player-walk",
-      frames: this.anims.generateFrameNumbers("player", { start: 19, end: 20 }),
-      frameRate: 3,
+      frames: this.anims.generateFrameNumbers("player_walk", { start: 0, end: 7 }),
+      frameRate: 14,
       repeat: -1
     });
   }
 
   setUpMap() {
-    // this.map = this.make.tilemap({ key: 'map' });
-    // var tiles = this.map.addTilesetImage('scifi_tilesheet', 'scifi_tilesheet_img');
-    // this.map.createStaticLayer('background_layer', tiles, 0, 0);
+    this.map = this.make.tilemap({ key: 'map' });
+    var tiles = this.map.addTilesetImage('scifi_tilesheet', 'scifi_tilesheet_img');
+
+    this.background_layer = this.map.createStaticLayer('background_layer', tiles, 0, 0);
+    // this.map.setCollisionBetween(20, 39);
+    this.map.setCollisionByProperty({ collidable: true });
   }
 
   setUpCamera() {
-    this.cameras.main
-        .setZoom(3);
+    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
 
-    var cursors = this.input.keyboard.createCursorKeys();
-    var controlConfig = {
-        camera: this.cameras.main,
-        left: cursors.left,
-        right: cursors.right,
-        up: cursors.up,
-        down: cursors.down,
-        speed: 0.5
-    };
-
-    this.cameraControls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
-
-    this.cameras.main.startFollow(this.player);
-  }
-
-  update (time, delta) {
-    this.cameraControls.update(delta);
+    this.cameras.main.startFollow(this.player, true, 0.8, 0.8);
   }
 }
