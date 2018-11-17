e46e9fc5ee6e8bf74ddd7b7c0dbf01a98dfc45f5
cubetastic <cubetastic33@gmail.com>
2018-11-17 07:05:35 +0530

    try to add ship sprite

---
+++ src/entities/ship.js
@@ -0,0 +1,13 @@
+import Phaser from 'phaser';
+
+class Ship extends Phaser.Physics.Arcade.Sprite {
+	constructor (scene, x, y, config) {
+		super(scene, x, y, config.key);
+
+		// Use the ship texture from title.js
+		this.setTexture('ship');
+		this.setPosition(x, y);
+	}
+}
+
+export default Ship;
