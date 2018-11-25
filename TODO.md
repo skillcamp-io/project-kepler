9fb2ea7b17ba95e0fee7ed131564caa5ce0dffa5
mixolydian251 <tjhmusic11@gmail.com>
2018-11-25 17:32:30 -0500

    add: enemy health bar and damage taken

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
 # TODO
 - [X] Add animation at plant.js [load a sprite at title.js?]
 - [X] Reduce size of the image sprite??
-- [ ] Collider doesnt work? (Between plant and player)
+- [ ] Collider does'nt work? (Between plant and player)
 - [ ] Add actions or events to the plant (idk how to do this - @renabil)
--- src/entities/ufo.js
+++ src/entities/ufo.js
@@ -1,8 +1,15 @@
 import Phaser from 'phaser';
+import HealthBar from './healthBar.js';
 
 class UFO extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, config) {
     super(scene, x, y,  config.key);
+
+    this.sfx = {
+      explode: new Audio('/assets/audio/explosion.mp3'),
+    };
+
+    this.alive = true;
 
     this.scene = scene;
     this.finder = this.scene.finder;
@@ -10,16 +17,70 @@ class UFO extends Phaser.Physics.Arcade.Sprite {
     this.scene.add.existing(this);
     this.scene.physics.add.existing(this);
 
-    this.anims.play('ufo-float', true);
+    this.hp = new HealthBar(scene, -20, -30);
+    this.container = this.scene.add.container(this.x, this.y, this.hp);
+
+    setTimeout(() => {
+      this.anims.play('ufo-float', true);
+    }, Math.floor(Math.random() * 500));
 
     this.velocity = 1000;
 
     this.findPath();
 
+    this.setInteractive();
+
+    this.on('pointerdown', (pointer) => {
+      this.setTint(0xff4d4d);
+      this.hp.decrease(20);
+      setTimeout(() => {
+        this.setTint(0xffffff)
+      },200);
+    });
   }
 
   preUpdate(time, delta) {
     super.preUpdate(time, delta);
+
+    this.container.x = this.x;
+    this.container.y = this.y;
+
+    if(this.alive && this.hp.value <= 0){
+      this.explode()
+    }
+  }
+
+  explode(){
+    this.sfx.explode.play();
+    this.alive = false;
+
+    this.scene.tweens.killTweensOf(this);
+
+    const particles = this.scene.add.particles('spark');
+
+    const emitter = particles.createEmitter({
+      "gravityY": -110,
+      "maxParticles": 0,
+      "angle": { "min": 0, "max": 360 },
+      "alpha": { "start": 1, "end": 0, "ease": "Linear" },
+      "lifespan": 900,
+      "maxVelocityX": 200,
+      "maxVelocityY": 200,
+      "x": this.x,
+      "y": this.y,
+      "speed": 400,
+      "scale": { "start": 0.6, "end": 0.4, "ease": "Linear" }
+    });
+    emitter.setBlendMode(Phaser.BlendModes.ADD);
+
+    console.log(emitter)
+    console.log(particles)
+
+    setTimeout(() => {
+      this.destroy();
+      this.hp.destroy();
+      emitter.visible = false;
+    }, 1000);
   }
 
   moveAlongPath(path) {
@@ -63,14 +124,12 @@ const spawnOnMapEdge = (scene) => {
     case 1:
       return { x: position, y: 0 };
     case 2:
-      return { x: scene.map.width - 1, y: position };
+      return { x: scene.map.width - 0, y: position };
     case 3:
-      return { x: position, y: scene.map.height - 1 };
+      return { x: position, y: scene.map.height - 0 };
     case 4:
       return { x: 0, y: position };
   }
 };
 
 export { UFO as default, spawnOnMapEdge };
-
-// TODO, add a healthbar: http://labs.phaser.io/edit.html?src=src\game%20objects\graphics\health%20bars%20demo.js
